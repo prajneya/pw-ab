0f5813bdfc29ecebac61d800bb2853d90e8c36c8
Prajneya Kumar <prajneya@gmail.com>
2024-02-23 15:12:38 +0530

    add all initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align="center">
+  Personal Website - Anushree Belwariar
+</h1>
+<p align="center">
+  This is a website template whose original owner is <a href="https://github.com/bchiang7">BChiang</a>
+</p>
---
+++ src/components/sections/hero.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+import { navDelay, loaderDelay } from '@utils';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  height: 100vh;
+  padding: 0;
+
+  @media (max-height: 700px) and (min-width: 700px), (max-width: 360px) {
+    height: auto;
+    padding-top: var(--nav-height);
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  h3 {
+    margin-top: 5px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 540px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <h1>Hi, my name is</h1>;
+  const two = <h2 className="big-heading">Anushree Belwariar.</h2>;
+  const three = <h3 className="big-heading">I craft and perfect narratives.</h3>;
+  const four = (
+    <>
+      <p>
+        I’m a highly motivated and creative content writer and editor with a passion for digital content creation, journalism, copywriting, and publishing. Currently enhancing my expertise while exploring the realms of SEO and user engagement, I thrive on solving the puzzle of connecting with audiences through compelling storytelling and insightful content.
+      </p>
+    </>
+  );
+  const five = (
+    <a
+      className="email-link"
+      href=""
+      target="_blank"
+      rel="noreferrer">
+      Read my Blog!
+    </a>
+  );
+
+  const items = [one, two, three, four, five];
+
+  return (
+    <StyledHeroSection>
+      {prefersReducedMotion ? (
+        <>
+          {items.map((item, i) => (
+            <div key={i}>{item}</div>
+          ))}
+        </>
+      ) : (
+        <TransitionGroup component={null}>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      )}
+    </StyledHeroSection>
+  );
+};
+
+export default Hero;
